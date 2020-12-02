ce1605abdca113154edd8efd96bbc05fae837d4f
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2020-12-02 18:26:16 +0300

    Add speech recognition function to display player's saying
    Change input tag with div. Because of that use box.textContent to check
    number and player's saying. When guessing is correct, true is written on
    the console.
    
    I should check whether player's saying is number or something else.
    Thus, just number is showing in the box.

--- script.js
+++ script.js
@@ -7,22 +7,43 @@ const box = document.querySelector('.box')
 //Select the button
 const button = document.querySelector('button')
 
+//Define speech recognition
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+//Binding
+const recognition = new SpeechRecognition();
+//interim result is false by default. If you change this variable to true, while user speaking, populating on the screen
+recognition.interimResults = true
 
-//Create a function that gives a random number between 0 and 100
-function randomNum(){
-return  num = Math.floor(Math.random()*100 + 1)
-//for max min,  Math.random *(max-min+1) + min
-}
+//Event Listener
+recognition.addEventListener('result', function (e) {
 
-//When player write his or her own guesses, show true 
-box.addEventListener('input', function(){
-    if(box.value==num){
-    alert(`Correct, it was ${num}`)
-}
+    const transcript = Array.from(e.results) //Because this is a list not an array
+        .map(result => result[0])
+        .map(result => result.transcript) //gives arrays
+        .join('') //turn array to string
+
+    console.log(transcript)
+    box.textContent = transcript
+
+    if (box.textContent == num) {
+        console.log('true')
+    }
 })
 
+recognition.addEventListener('end', recognition.start) //When speaking end, start again the start function
+
+recognition.start() //Without this function we cannot see anything
+
+
+//Create a function that gives a random number between 0 and 100
+function randomNum() {
+    return num = Math.floor(Math.random() * 100 + 1)
+    //for max min,  Math.random *(max-min+1) + min
+}
+
+
 //start function
-function startGame(){
+function startGame() {
     console.log(randomNum())
 }
 
