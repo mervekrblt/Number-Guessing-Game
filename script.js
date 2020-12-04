af27b41862aedeea57ca289c5c7c3971efe5887e
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2020-12-04 16:03:27 +0300

    Fix button display and add hint button
    When player click the start button, add display:none property to hidden
    this. If guessing number is correct, play again button will shown.
    
    I added hint button also. Without starting start button, it doesnt work,
    because num is undefined.
    
    I used ES6  feature in hint function. I used a lot of if statements I
    dont know why :)

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const box = document.querySelector('.box')
-const button = document.querySelector('button')
+const button = document.querySelector('.start')
+const hint = document.querySelector('.hint')
 const text1 = document.querySelector('.text1')
-const text2 =  document.querySelector('.text2')
+const text2 = document.querySelector('.text2')
 let num;
 
 
 //Define speech recognition
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
 //Binding
-const recognition = new SpeechRecognition();
 //interim result is false by default. If you change this variable to true, while user speaking, populating on the screen
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+const recognition = new SpeechRecognition();
 recognition.interimResults = true
 
-//Event Listener
-recognition.addEventListener('result', function (e) {
 
+function showNum(e) {
     const transcript = Array.from(e.results) //Because this is a list not an array
         .map(result => result[0])
         .map(result => result.transcript) //gives arrays
@@ -22,25 +22,27 @@ recognition.addEventListener('result', function (e) {
 
     let isNumber = Number(transcript)
 
-    if(Number.isNaN(isNumber)){ //dont use isNumber===NaN, use Number.isNaN()
-      {text1.textContent = 'Not a Number', box.textContent=''}
-    } else {box.textContent = transcript, text1.textContent = ''}
+    if (Number.isNaN(isNumber)) { //dont use isNumber===NaN, use Number.isNaN()
+        text1.textContent = 'Not a Number'
+        box.textContent = ''
+    } else {
+        box.textContent = transcript
+        text1.textContent = ''
+    }
 
-    
-    if(isNumber>num){
-       return text1.textContent = 'Go Lower'
-    }else if(isNumber<num){
+
+    if (isNumber > 100 || isNumber < 0) {
+        return box.textContent = '', text1.textContent = 'Number should be between 0-100'
+    }
+
+    if (isNumber > num) {
+        return text1.textContent = 'Go Lower'
+    } else if (isNumber < num) {
         return text1.textContent = 'Go Higher'
-    } else if(isNumber===num)
-        return text1.textContent = 'Congratulations'
-     
-
-})
-
-recognition.addEventListener('end', recognition.start) //When speaking end, start again the start function
-
-recognition.start() //Without this function we cannot see anything
-
+    } else if (isNumber === num) {
+        return text1.textContent = '🎉CONGRATULATIONS🎉', button.style.display = 'block', button.textContent = 'Play Again'
+    }
+}
 
 //Create a function that gives a random number between 0 and 100
 function randomNum() {
@@ -49,9 +51,20 @@ function randomNum() {
 }
 
 
-//start function
+//When player click the start; create a number, hidden start button, reset hint button
 function startGame() {
-    console.log(randomNum())
+    randomNum()
+    console.log(num)
+    recognition.addEventListener('result', showNum)
+    button.style.display = 'none'
+    text1.textContent = ''
+    box.textContent = ''
+    hint.innerHTML = 'Hint!'
 }
 
 button.addEventListener('click', startGame)
+recognition.addEventListener('end', recognition.start) //When speaking end, start again the start function
+recognition.start() //Without this function we cannot see anything
+hint.addEventListener('click', function () {
+    return num === undefined ? '' : this.innerHTML = num
+})
